b96225390815abb179f21b69df282645734a06a2
chaitanyakumarchinnam <chaitanyakumarchinnam@192.168.0.21>
2021-01-09 14:22:44 -0500

    First Commit

---
+++ src/main/java/com/niit/vm/VendingMachineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.niit.vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niit/vm/VendingMachineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
